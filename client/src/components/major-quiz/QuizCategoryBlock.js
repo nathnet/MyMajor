dc5bb9fce33fc36392e4522d29d37d757cd51b95
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2020-08-28 18:56:27 -0700

    Fixed navbar colors, and link for major quizzes

--- client/src/components/major-quiz/QuizCategoryBlock.js
+++ client/src/components/major-quiz/QuizCategoryBlock.js
@@ -13,15 +13,15 @@ const QuizCategoryBlock = (props) => {
     const thisLink = link ? link : category.toLowerCase();
 
     return(
-        <RRNavLink className="icon" to={`/major-quiz/${thisLink}`}>
-            <div className={`d-inline-block px-0 pt-2 pb-3 ${className}`}>
+        <div className={`d-inline-block px-0 pt-2 pb-3 ${className}`}>
+            <RRNavLink className="icon" to={`/major-quiz/${thisLink}`}>    
                 <div 
                     className={`${float ? "col-md-11" : "col-12"} py-4 text-black shadow-br ` +
                         `border border-1 border-theme-light text-center clearfix ${float}`}>
                     {category}
                 </div>
-            </div>
-        </RRNavLink>
+            </RRNavLink>
+        </div>
     );
 }
 
